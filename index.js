da5457a0d2823975c3687b0240e0b7e9a22d6f70
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-08 20:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// const app = "I don't do much."
-//
-// var kittens = ["Milo", "Otis", "Garfield"]
-//
+const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
 // function destructivelyAppendKitten(name){
 //   kittens.push(name);
 //   return kittens;
